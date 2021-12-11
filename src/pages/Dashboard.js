b4b20107d73bb5f10ad60aea6d3f0b31183c6577
leilali0 <ml7075@nyu.edu>
2021-12-11 15:36:52 -0500

    CORS problem

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PostCard from "../components/PostCard";
+import axios from "axios";
 
 export const MOCK_DATA = [
   {
@@ -11,7 +12,25 @@ export const MOCK_DATA = [
   },
 ];
 
+const url = `http://localhost:4000`;
+
 function Dashboard() {
+  const [post, setPost] = useState();
+
+  // Getting all posts
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(function (response) {
+        console.log({ response });
+        //setPost(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.warn(error);
+      });
+  }, []);
+
   return (
     <div className="PageWrapper">
       <h1>Dashboard</h1>
